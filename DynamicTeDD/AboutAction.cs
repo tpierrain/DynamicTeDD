2a3569c8a73dac1715e3933ae2de190fb00f47c0
Thomas PIERRAIN <thomas@pierrain.net>
2014-01-06 08:35:03 +0100

    Adds reference to the Apache license.

--- DynamicTeDD/AboutAction.cs
+++ DynamicTeDD/AboutAction.cs
@@ -1,25 +1,40 @@
-using System.Windows.Forms;
-using JetBrains.ActionManagement;
-using JetBrains.Application.DataContext;
-
+// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="AboutAction.cs" company="">
+// //   Copyright 2014 Thomas PIERRAIN
+// //   Licensed under the Apache License, Version 2.0 (the "License");
+// //   you may not use this file except in compliance with the License.
+// //   You may obtain a copy of the License at
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //   Unless required by applicable law or agreed to in writing, software
+// //   distributed under the License is distributed on an "AS IS" BASIS,
+// //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //   See the License for the specific language governing permissions and
+// //   limitations under the License.
+// // </copyright>
+// // --------------------------------------------------------------------------------------------------------------------
 namespace DynamicTeDD
 {
-  [ActionHandler("DynamicTeDD.About")]
-  public class AboutAction : IActionHandler
-  {
-    public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
-    {
-      // return true or false to enable/disable this action
-      return true;
-    }
+    using System.Windows.Forms;
 
-    public void Execute(IDataContext context, DelegateExecute nextExecute)
+    using JetBrains.ActionManagement;
+    using JetBrains.Application.DataContext;
+
+    [ActionHandler("DynamicTeDD.About")]
+    public class AboutAction : IActionHandler
     {
-      MessageBox.Show(
-        "DynamicTeDD\nThomas PIERRAIN, from Rui CARVALHO's original idea\n\nA ReSharper plug-in to make your TDD more fluent via dynamic usage. 1/ You start defining your contract using a dynamic 'modeling clay' instance from within a test (compiling RED) 2/ The plugin replace your dynamic instance with a non-dynamic skeleton code (RED) 3/ You implement the dynamic skeleton code to make your test pass (GREEN).",
-        "About DynamicTeDD",
-        MessageBoxButtons.OK,
-        MessageBoxIcon.Information);
+        public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
+        {
+            // return true or false to enable/disable this action
+            return true;
+        }
+
+        public void Execute(IDataContext context, DelegateExecute nextExecute)
+        {
+            MessageBox.Show(
+            "DynamicTeDD\nThomas PIERRAIN, from Rui CARVALHO's original idea\n\nA ReSharper plug-in to make your TDD more fluent via dynamic usage. 1/ You start defining your contract using a dynamic 'modeling clay' instance from within a test (compiling RED) 2/ The plugin replace your dynamic instance with a non-dynamic skeleton code (RED) 3/ You implement the dynamic skeleton code to make your test pass (GREEN).", 
+            "About DynamicTeDD", 
+            MessageBoxButtons.OK, 
+            MessageBoxIcon.Information);
+        }
     }
-  }
 }
--- DynamicTeDD/SkeletonTheDynamicContextAction.cs
+++ DynamicTeDD/SkeletonTheDynamicContextAction.cs
@@ -1,19 +1,33 @@
-using System;
-using JetBrains.Application.Progress;
-using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Feature.Services.Bulbs;
-using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
-using JetBrains.ReSharper.Feature.Services.LinqTools;
-using JetBrains.ReSharper.Intentions.Extensibility;
-using JetBrains.ReSharper.Intentions.Extensibility.Menu;
-using JetBrains.ReSharper.Psi.CSharp;
-using JetBrains.ReSharper.Psi.CSharp.Tree;
-using JetBrains.ReSharper.Psi.Tree;
-using JetBrains.TextControl;
-using JetBrains.Util;
-
+// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="SkeletonTheDynamicContextAction.cs" company="">
+// //   Copyright 2014 Thomas PIERRAIN
+// //   Licensed under the Apache License, Version 2.0 (the "License");
+// //   you may not use this file except in compliance with the License.
+// //   You may obtain a copy of the License at
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //   Unless required by applicable law or agreed to in writing, software
+// //   distributed under the License is distributed on an "AS IS" BASIS,
+// //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //   See the License for the specific language governing permissions and
+// //   limitations under the License.
+// // </copyright>
+// // --------------------------------------------------------------------------------------------------------------------
 namespace DynamicTeDD
 {
+    using System;
+
+    using JetBrains.Application.Progress;
+    using JetBrains.ProjectModel;
+    using JetBrains.ReSharper.Feature.Services.Bulbs;
+    using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
+    using JetBrains.ReSharper.Feature.Services.LinqTools;
+    using JetBrains.ReSharper.Intentions.Extensibility;
+    using JetBrains.ReSharper.Psi.CSharp;
+    using JetBrains.ReSharper.Psi.CSharp.Tree;
+    using JetBrains.ReSharper.Psi.Tree;
+    using JetBrains.TextControl;
+    using JetBrains.Util;
+
     [ContextAction(Name = "SkeletonTheDynamic", Description = "Replaces the dynamic instance with a non-dynamic skeleton type instance.", Group = "C#")]
     public class SkeletonTheDynamicAction : ContextActionBase, IContextAction
     {
@@ -38,6 +52,7 @@ namespace DynamicTeDD
                     return true;
                 }
             }
+
             return false;
         }
 
@@ -62,84 +77,84 @@ namespace DynamicTeDD
         }
     }
 
-    //[ContextAction(Description = "My skeleton the dynamic context action does ...",
-    //  Group = "C#",
-    //  Name = "My skeleton the dynamic Context Action")]
-    //public sealed class SkeletonTheDynamicContextAction : IContextAction
-    //{
-    //    private readonly ICSharpContextActionDataProvider _provider;
-    //    private IBulbAction[] _items;
+    // [ContextAction(Description = "My skeleton the dynamic context action does ...",
+    // Group = "C#",
+    // Name = "My skeleton the dynamic Context Action")]
+    // public sealed class SkeletonTheDynamicContextAction : IContextAction
+    // {
+    // private readonly ICSharpContextActionDataProvider _provider;
+    // private IBulbAction[] _items;
 
-    //    /// <summary>
-    //    /// For languages other than C# any inheritor of <see cref="IContextActionDataProvider"/> can 
-    //    /// be injected in this constructor.
-    //    /// </summary>
-    //    public SkeletonTheDynamicContextAction(ICSharpContextActionDataProvider provider)
-    //    {
-    //        _provider = provider;
-    //    }
+    // /// <summary>
+    // /// For languages other than C# any inheritor of <see cref="IContextActionDataProvider"/> can 
+    // /// be injected in this constructor.
+    // /// </summary>
+    // public SkeletonTheDynamicContextAction(ICSharpContextActionDataProvider provider)
+    // {
+    // _provider = provider;
+    // }
 
-    //    public void CreateBulbItems(BulbMenu menu)
-    //    {
-    //        // Basically 
-    //        foreach (var item in Items)
-    //        {
-    //            // use other extension methods on BulbMenu to arrange menu items.
-    //            menu.ArrangeContextAction(item);
-    //        }
-    //    }
+    // public void CreateBulbItems(BulbMenu menu)
+    // {
+    // // Basically 
+    // foreach (var item in Items)
+    // {
+    // // use other extension methods on BulbMenu to arrange menu items.
+    // menu.ArrangeContextAction(item);
+    // }
+    // }
 
-    //    public bool IsAvailable(IUserDataHolder cache)
-    //    {
-    //        // Availability code may be optimized but for most cases can be as simple as follow:
-    //        return Items.Length > 0;
-    //    }
+    // public bool IsAvailable(IUserDataHolder cache)
+    // {
+    // // Availability code may be optimized but for most cases can be as simple as follow:
+    // return Items.Length > 0;
+    // }
 
-    //    private IBulbAction[] Items
-    //    {
-    //        get
-    //        {
-    //            if (_items == null)
-    //            {
-    //                _items = new IBulbAction[]
-    //               {
-    //                 new MyBulbItem(_provider)
-    //               };
-    //            }
-    //            return _items;
-    //        }
-    //    }
-    //}
+    // private IBulbAction[] Items
+    // {
+    // get
+    // {
+    // if (_items == null)
+    // {
+    // _items = new IBulbAction[]
+    // {
+    // new MyBulbItem(_provider)
+    // };
+    // }
+    // return _items;
+    // }
+    // }
+    // }
 
-    //public class MyBulbItem : BulbActionBase
-    //{
-    //    private readonly ICSharpContextActionDataProvider _provider;
+    // public class MyBulbItem : BulbActionBase
+    // {
+    // private readonly ICSharpContextActionDataProvider _provider;
 
-    //    public MyBulbItem(ICSharpContextActionDataProvider provider)
-    //    {
-    //        _provider = provider;
-    //    }
+    // public MyBulbItem(ICSharpContextActionDataProvider provider)
+    // {
+    // _provider = provider;
+    // }
 
-    //    public override string Text
-    //    {
-    //        get
-    //        {
-    //            // text returned here will be displayed on the context action
-    //            return "Skeleton the dynamic Context action text";
-    //        }
-    //    }
+    // public override string Text
+    // {
+    // get
+    // {
+    // // text returned here will be displayed on the context action
+    // return "Skeleton the dynamic Context action text";
+    // }
+    // }
 
-    //    protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
-    //    {
-    //        // put transacted steps here
-    //        // use 'provider' field to get currently selected element
+    // protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+    // {
+    // // put transacted steps here
+    // // use 'provider' field to get currently selected element
 
-    //        return textControl =>
-    //        {
-    //            // put post-transaction steps here
-    //            // if there are none, you can replace this with 'return null'
-    //        };
-    //    }
+    // return textControl =>
+    // {
+    // // put post-transaction steps here
+    // // if there are none, you can replace this with 'return null'
+    // };
+    // }
 
-    //}
+    // }
 }
